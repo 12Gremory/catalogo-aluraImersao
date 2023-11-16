183a9c0e0b13436fe1babe94180bbfc89222fab9
Thomas <thomas0369@hotmail.com>
2023-11-16 18:29:18 -0300

    validação da url

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ var listaFilmes = [];
 
 function adicionarFilme() {
   var filme = document.getElementById("filme").value;
-  listaFilmes.push(filme);
-  const list = document.getElementById("listaFilmes");
-  const task = document.createElement("img");
-
-  for (let i = 0; i < listaFilmes.length; i++) task.src = `${listaFilmes[i]}`;
-
-  list.appendChild(task);
   var inputFilme = document.getElementById("filme");
-  inputFilme.value = "";
+  if (isImage(filme)) {
+    listaFilmes.push(filme);
+    const list = document.getElementById("listaFilmes");
+    const task = document.createElement("img");
+
+    for (let i = 0; i < listaFilmes.length; i++) task.src = `${listaFilmes[i]}`;
+
+    list.appendChild(task);
+    inputFilme.value = "";
+  } else {
+    alert("URL inválida");
+    inputFilme.value = "";
+  }
+}
+
+function isImage(url) {
+  return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
 }
