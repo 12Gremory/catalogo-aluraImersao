ea59781875c6fcbd15de05b30f5aefe6e8da0de7
Thomas <thomas0369@hotmail.com>
2023-11-16 18:11:43 -0300

    função para adição de n filmes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var filme1 =
-  "https://upload.wikimedia.org/wikipedia/pt/1/1b/Schoolrockposter.jpg";
-var filme2 =
-  "https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg";
-var filme3 =
-  "https://upload.wikimedia.org/wikipedia/pt/1/1d/Harry_Potter_Pedra_Filosofal_2001.jpg";
-
 var listaFilmes = [];
-listaFilmes.push(filme1, filme2, filme3);
 
-for (let i = 0; i < listaFilmes.length; i++)
-  document.write("<img src=" + listaFilmes[i] + ">");
+function adicionarFilme() {
+  var filme = document.getElementById("filme").value;
+  listaFilmes.push(filme);
+  const list = document.getElementById("listaFilmes");
+  const task = document.createElement("img");
+
+  for (let i = 0; i < listaFilmes.length; i++) task.src = `${listaFilmes[i]}`;
+
+  list.appendChild(task);
+}
